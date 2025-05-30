888cda86eea243a6d37b9d62b0b9dacc74f4c018
THOMASLOVEJESUS\thomasLoveJesus <thomasniu@gmail.com>
2025-05-30 16:47:30 +0800

    Finished new data layer refactoring

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask
+from data import jokes
 import random
 
 app = Flask(__name__)
 
-jokes = [
-    "Relationship status? I'll leave the relations to the database.",
-    "How do you get the code for the bank vault? You checkout their branch.",
-    "How did the developer announce their engagement? They returned true!"
-]
+
 
 @app.get("/")
-def hello_world():
+def tell_a_joke():
     joke = random.choice(jokes)
     return f"<p>{joke}</p>"
---
+++ data.py
@@ -0,0 +1,5 @@
+jokes = [
+    "Relationship status? I'll leave the relations to the database.",
+    "How do you get the code for the bank vault? You checkout their branch.",
+    "How did the developer announce their engagement? They returned true!"
+]
